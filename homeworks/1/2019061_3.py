a8912b9593aaa2f22416e5fb93f8f12b7c671787
Mihir Chaturvedi <plibither8@gmail.com>
2021-12-05 18:13:40 +0530

    feat: Endsem

--- homeworks/1/2019061_3.py
+++ homeworks/1/2019061_3.py
@@ -1,23 +1,24 @@
 # Mihir Chaturvedi
 # Roll number: 2019061
 
-import gmpy2
 import time
+
+import gmpy2
 
 
 def bob_sends_alice(p, g):
     b = gmpy2.mpz_random(gmpy2.random_state(time.time_ns()), 10 ** 1023)
     B = gmpy2.powmod(g, b, p)
-    print("b =", b)
-    print("B =", B)
+    # print("b =", b)
+    # print("B =", B)
     return (B, b)
 
 
 def alice_sends_bob(p, g):
     a = gmpy2.mpz_random(gmpy2.random_state(time.time_ns()), 10 ** 1023)
     A = gmpy2.powmod(g, a, p)
-    print("a =", a)
-    print("A =", A)
+    # print("a =", a)
+    # print("A =", A)
     return (A, a)
 
 
